c3f97599919c65b3457304884e53434125a388d2
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-04-03 00:34:25 +0300

    update

--- my-app/src/components/MediaRow.jsx
+++ my-app/src/components/MediaRow.jsx
@@ -2,24 +2,24 @@ import PropTypes from 'prop-types';
 import { mediaUrl } from '../utils/variables';
 
 const MediaRow = ({ file }) => {
-    return (
-        <tr>
-            <td>
-                <img src={mediaUrl + file.thumbnails.w160} alt={file.title} />
-            </td>
-            <td>
-                <h3>{file.title}</h3>
-                <p>{file.description}</p>
-            </td>
-            <td>
-                <a href={file.filename}>View</a>
-            </td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>
+        <img src={mediaUrl + file.thumbnails.w160} alt={file.title} />
+      </td>
+      <td>
+        <h3>{file.title}</h3>
+        <p>{file.description}</p>
+      </td>
+      <td>
+        <a href={mediaUrl + file.filename}>View</a>
+      </td>
+    </tr>
+  );
 };
 
 MediaRow.propTypes = {
-    file: PropTypes.object.isRequired,
+  file: PropTypes.object.isRequired,
 };
 
 export default MediaRow;
--- my-app/src/components/MediaTable.jsx
+++ my-app/src/components/MediaTable.jsx
@@ -1,40 +1,21 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
+import { useMedia } from '../hooks/ApiHooks';
 import { baseUrl } from '../utils/variables';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
-    const [mediaArray, setMediaArray] = useState([]);
-    const getMedia = async () => {
-        const response = await fetch(baseUrl + 'media');
-        const files = await response.json();
-        const filesWithThumbnail = await Promise.all(
-            files.map(async (file) => {
-                const response = await fetch(baseUrl + 'media/' + file.file_id);
-                return await response.json();
-            })
-        );
-        setMediaArray(filesWithThumbnail);
-    };
 
-    useEffect(() => {
-        try {
-            getMedia();
-        } catch (error) {
-            console.log(error.message);
-        }
-    }, []);
-
-    console.log(mediaArray);
-    return (
-        <table>
-            <tbody>
-                {mediaArray.map((item, index) => {
-                    return <MediaRow key={index} file={item} />;
-                })}
-            </tbody>
-        </table>
-    );
+  const { mediaArray } = useMedia();
+  return (
+    <table>
+      <tbody>
+        {mediaArray.map((item, index) => {
+          return <MediaRow key={index} file={item} />;
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 MediaTable.propTypes = {};
